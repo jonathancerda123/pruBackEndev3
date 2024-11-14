c436c5ea65fa40b52eac22d516425c61ced503f9
jonathancerda123 <jonathanygnaciocerdagarrido@gmail.com>
2024-11-13 22:08:20 -0300

    models editado y actualizado

--- galeriaMusica/models.py
+++ galeriaMusica/models.py
@@ -5,7 +5,7 @@ class Imagen(models.Model):
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField()
     imagen = models.ImageField(upload_to='imagenes/')
-    autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='imagenes_galeria_artistas')
+    compositor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='imagenes_galeria_musica')
     fecha_subida = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
