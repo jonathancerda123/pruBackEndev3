cbb0e073763e83a77bc790bf83b47eaecd0cbf96
AntoCon <constanza.contreras55@inacapmail.cl>
2024-11-14 01:15:41 -0300

    settings

--- backev3/urls.py
+++ backev3/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings 
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,7 @@ urlpatterns = [
     path('productos/', include('productos.urls', namespace='productos')),
     path('zapatillas/', include('galeriaZapatillas.urls', namespace='galeriaZapatillas')),
 ]
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+
