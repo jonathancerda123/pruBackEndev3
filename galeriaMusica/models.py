757ac8d98be404307570d8ba689605a23a556fc1
jonathancerda123 <jonathanygnaciocerdagarrido@gmail.com>
2024-11-13 22:01:46 -0300

    app galeria musica con sus migraciones correctas y corregidas

--- galeriaMusica/models.py
+++ galeriaMusica/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Imagen(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='imagenes/')
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='imagenes_galeria_artistas')
+    fecha_subida = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
