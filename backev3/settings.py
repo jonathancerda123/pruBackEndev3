e5fb731e0e6998399691843e079337590246fe2e
jonathancerda123 <jonathanygnaciocerdagarrido@gmail.com>
2024-11-24 23:30:23 -0300

    intento de solucion a la base de datos

--- backev3/settings.py
+++ backev3/settings.py
@@ -88,8 +88,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 print(config('DB_USER'))
